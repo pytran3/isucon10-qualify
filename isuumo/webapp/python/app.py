de48436b18de62b49aff2f07bf02e3d32ac2ac2c
pytran <34180469+pytran3@users.noreply.github.com>
2020-09-12 19:24:20 +0900

    動くようにリバーと

--- isuumo/webapp/python/app.py
+++ isuumo/webapp/python/app.py
@@ -338,26 +338,26 @@ def post_estate_nazotte():
             ),
         )
         estates = cur.fetchall()
-        # estates_in_polygon = []
-        # for estate in estates:
-        #     query = "SELECT * FROM estate WHERE id = %s AND ST_Contains(ST_PolygonFromText(%s), ST_GeomFromText(%s)) limit 1"
-        #     polygon_text = (
-        #         f"POLYGON(({','.join(['{} {}'.format(c['latitude'], c['longitude']) for c in coordinates])}))"
-        #     )
-        #     geom_text = f"POINT({estate['latitude']} {estate['longitude']})"
-        #     cur.execute(query, (estate["id"], polygon_text, geom_text))
-        #     if len(cur.fetchall()) > 0:
-        #         estates_in_polygon.append(estate)
-        query = "SELECT * FROM estate WHERE id in (" + ",".join([e["id"] for e in estates]) + ") %s AND ST_Contains(ST_PolygonFromText(%s), ST_GeomFromText(POINT(`latitude`, `longitude` )))"
-        polygon_text = (
-            f"POLYGON(({','.join(['{} {}'.format(c['latitude'], c['longitude']) for c in coordinates])}))"
-        )
-        cur.execute(query, (polygon_text, ))
-        estates = cur.fetchall()
+        estates_in_polygon = []
+        for estate in estates:
+            query = "SELECT * FROM estate WHERE id = %s AND ST_Contains(ST_PolygonFromText(%s), ST_GeomFromText(%s)) limit 1"
+            polygon_text = (
+                f"POLYGON(({','.join(['{} {}'.format(c['latitude'], c['longitude']) for c in coordinates])}))"
+            )
+            geom_text = f"POINT({estate['latitude']} {estate['longitude']})"
+            cur.execute(query, (estate["id"], polygon_text, geom_text))
+            if len(cur.fetchall()) > 0:
+                estates_in_polygon.append(estate)
+        # query = "SELECT * FROM estate WHERE id in (" + ",".join([e["id"] for e in estates]) + ") %s AND ST_Contains(ST_PolygonFromText(%s), ST_GeomFromText(POINT(`latitude`, `longitude` )))"
+        # polygon_text = (
+        #     f"POLYGON(({','.join(['{} {}'.format(c['latitude'], c['longitude']) for c in coordinates])}))"
+        # )
+        # cur.execute(query, (polygon_text, ))
+        # estates = cur.fetchall()
     finally:
         cnx.close()
 
-    estates = [camelize(estate) for estate in estates[:NAZOTTE_LIMIT]]
+    estates = [camelize(estate) for estate in estates_in_polygon[:NAZOTTE_LIMIT]]
     return {"estates": estates, "count": len(estates)}
 
 @app.route("/api/estate/<int:estate_id>", methods=["GET"])
