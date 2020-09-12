85977438601ca3fdf4775aae5144a7042132dee1
yumechi <yumechi0525+isucon@gmail.com>
2020-09-12 09:35:28 +0000

    コピースクリプトの改良

--- copy_app.sh
+++ copy_app.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-cp isuumo/webapp/python/app.py ~/isuumo/webapp/python/app.py
-cp isuumo/webapp/mysql/db/0_Schema.sql ~/isuumo/webapp/mysql/db/0_Schema.sql
+cp -v isuumo/webapp/python/app.py ~/isuumo/webapp/python/app.py
+cp -v isuumo/webapp/mysql/db/*.sql ~/isuumo/webapp/mysql/db/
 
